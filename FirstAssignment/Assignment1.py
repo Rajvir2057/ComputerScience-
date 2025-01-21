908c76d1f2c55fc3b2b07ae2c74d272a44313a5b
Rajvir2057 <rajvirkaur2057@gmail.com>
2025-01-21 07:56:16 -0800

    edited work

--- FirstAssignment/Assignment1.py
+++ FirstAssignment/Assignment1.py
@@ -1,1 +1,23 @@
 #Task one 
+Comb = "+--+--+--+ \n|  |  |  |"
+Line = "+--+--+--+"
+i = 1
+
+while i < 4:
+    print(Comb)
+    i=i+1
+
+print(Line)
+
+#Task two a vending machine task that returns change value.
+
+#Define constants.
+PENNIES_PER_DOLLAR =100
+PENNIES_PER_QUATER=25
+
+#Obtain input from user 
+UserInput=input("Enter bill value eg 1 = $1 bill: ")
+BillValue = int(UserInput)
+Userinput=("Enter itemprice in pennies")
+Itemprice=int(Userinput)
+
