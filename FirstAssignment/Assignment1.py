414315403955c5f32ff9a14e922774063e0934da
Rajvir2057 <rajvirkaur2057@gmail.com>
2025-01-22 11:09:38 -0800

    edited work

--- FirstAssignment/Assignment1.py
+++ FirstAssignment/Assignment1.py
@@ -18,17 +18,18 @@ PENNIES_PER_QUATER=25
 #Obtain input from user 
 UserInput=input("Enter bill value eg 1 = $1 bill: ")
 BillValue = int(UserInput)
-Userinput=("Enter itemprice in pennies: ")
+Userinput=input("Enter itemprice in pennies: ")
 Itemprice=int(Userinput)
 
 #compute change due
 
 changeDue = BillValue * PENNIES_PER_DOLLAR - Itemprice
 dollarcoins = changeDue // PENNIES_PER_DOLLAR
+changeDue = changeDue % PENNIES_PER_DOLLAR
 quaters= changeDue // PENNIES_PER_QUATER
 
 #Print change due
-print("Dollar coins :%6d" % changeDue)
-print("Quaters :%6d" % changeDue)
+print("Dollar coins : %6d" % changeDue)
+print("Quaters : %6d" % changeDue)
 
 ## Rajvir kaur 816815
